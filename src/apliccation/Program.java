2701b2e371b90dfae35303625e8d7af3ffe559ea
Monique Lima <monique_tamara@hotmail.com>
2022-11-27 17:45:33 -0300

    Chess layer and printing the board

--- src/apliccation/Program.java
+++ src/apliccation/Program.java
@@ -1,10 +1,13 @@
 package apliccation;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Monique");
+	ChessMatch chessMatch = new ChessMatch();
+	UI.printBoard(chessMatch.getPieces());
 		
 	}
 
